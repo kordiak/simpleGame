163c762ef73c095adffa7ad20514f13fa3a3ee52
kordiak <szymon.kordiaczynski@gmail.com>
2014-10-13 22:41:57 +0200

    Created base class "Character"

---
+++ Corona/code/classes/Character.lua
@@ -0,0 +1,37 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: acer
+-- Date: 2014-10-13
+-- Time: 22:20
+-- To change this template use File | Settings | File Templates.
+--
+
+local Character = {};
+
+--[[
+    --skin --width --height --type --xPostion --yPosition
+--]]
+Character.new = function(params)
+    local skin = params.skin or nil;
+    local width = params.width or 90;
+    local height = params.width or 90;
+    local type = params.type or "empty";
+    local xPosition = params.xPosition or 0;
+    local yPosition = params.yPosition or 0;
+
+
+    local object = display.newImageRect(skin, width, height);
+    object.x = xPosition;
+    object.y = yPosition;
+    object.type = type;
+
+    object.removeMe = function()
+        object:removeSelf();
+        --//todo:Consider if it is necessary
+    end
+
+
+    return object;
+end
+
+return Character
