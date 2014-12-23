103ae9bb7dddeb6f5559a696a2ad2186a32fd1a1
Bartosz <dirindon@gmail.com>
2014-12-23 11:11:39 +0100

    added some files

--- Corona/code/boardLibrary/forestGeneratorHelper.lua
+++ Corona/code/boardLibrary/forestGeneratorHelper.lua
@@ -1,18 +1,7 @@
 local properties = require("code.global.properties")
 
---
--- Created by IntelliJ IDEA.
--- User: acer
--- Date: 2014-10-13
--- Time: 22:20
--- To change this template use File | Settings | File Templates.
---
-
 local forestGeneratorHelper = {};
 
---[[
-    --skin --width --height --type --xPostion --yPosition --currentHex
---]]
 forestGeneratorHelper.new = function()
 
     forestGeneratorHelper.tab = {}
